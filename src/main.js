83b301d788706e1135b1a4895822b9455587d446
Maynard Lee <lxd_ddcome@163.com>
2018-04-26 19:49:06 +0800

    update home page

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 解决移动端按钮点击延时数秒300ms的问题.
+import fastClick from 'fastclick'
+// 移动端解决兼容问题方案.
+import './assets/styles/reset.css'
+// 1像素解决方案.
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
